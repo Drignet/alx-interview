367ee267aca3198cc3b732ab347412b40596d438
drignet <odibehenry@gmail.com>
2023-04-06 14:41:04 -0700

    minimum script

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Minimum Operations Script
+"""
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
